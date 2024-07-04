a0ea9493aef2f0f278989cbe2f4625ba14962a9d
Elvin Eduardo Cooper Gonzalez <ECOOPER@crcltd.com.do>
2024-07-04 16:55:37 -0400

    Reestructurando las plantillas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask('__name__')
 
@@ -6,9 +6,19 @@ app = Flask('__name__')
 def home():
     return render_template('index.html')
 
-@app.route('/prueba')
-def prueba():
-    return render_template('home.html')
+@app.route('/formulario')
+def formulario():
+    return render_template('form.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 
 if app == '__main__':
